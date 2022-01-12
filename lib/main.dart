2887ec4469914eef7509604167ca1bb1c712347c
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-12 07:31:57 +0100

    made the quiz brain class private

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(questionNumber),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -68,8 +68,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
                 bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
-
+                    quizBrain.getQuestionAnswer(questionNumber);
                 if (correctAnswer == true) {
                   print("user got it right");
                   // scoreKeeper.add(
@@ -109,7 +108,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
+                    quizBrain.getQuestionAnswer(questionNumber);
 
                 if (correctAnswer == false) {
                   print('user got it right');
